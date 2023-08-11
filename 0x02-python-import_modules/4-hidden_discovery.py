4a26fa65959e048dfca503064ad1cbbca21380b0
ericbigalito <emugarz@gmail.com>
2023-08-11 14:05:51 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-import hidden_4
+
 if __name__ == "__main__":
-    for i in range(len(dir(hidden_4))):
-        if dir(hidden_4)[i][:2] != "__":
-            print(dir(hidden_4)[i])
+    """Print all names defined by hidden_4 module"""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
