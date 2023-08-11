f728d088694e2f76beef94ca7bf7169559658cc4
ericbigalito <emugarz@gmail.com>
2023-08-11 14:13:48 -0700

    task 5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-from variable_load_5 import a
+def main():
+    from variable_load_5 import a
+    print("Value of 'a':", a)
+
 if __name__ == "__main__":
-    print(a)
+    main()
+
